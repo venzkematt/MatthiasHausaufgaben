8357a86b259e44edd495940b0b5e3321917202ba
venzkematt <115979637+venzkematt@users.noreply.github.com>
2023-01-20 19:52:51 +0100

    Delete test.js

--- JavaScript/test.js
+++
@@ -1,9 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/posts/3")
-    .then(response => response.json() )
-    .then(json => document.getElementById("km").innerHTML = json.title)
-    .catch(error => console.log(error))
-
-fetch("https://jsonplaceholder.typicode.com/photos/3")
-    .then(response => response.json() )
-    .then(json => document.getElementById("img").src = json.url)
-    .catch(error => console.log(error))
